95b5268b12fc74636b3eb923ef20ee7e9838f31b
Peter Spada <spadapet@outlook.com>
2021-11-14 07:52:55 -0800

    Initial commit

---
+++ PhotoMaster/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using PhotoMaster.Models;
+
+namespace PhotoMaster
+{
+    internal sealed partial class MainWindow : Window
+    {
+        public MainViewModel ViewModel { get; }
+
+        public MainWindow()
+        {
+            this.ViewModel = new MainViewModel();
+            this.InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs args)
+        {
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs args)
+        {
+        }
+    }
+}
---
+++ PhotoMaster/Models/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+using WpfTools;
+
+namespace PhotoMaster.Models
+{
+    internal class MainViewModel : WpfTools.PropertyNotifier
+    {
+        private DelegateCommand testAuthCommand;
+        public ICommand TestAuthCommand => this.testAuthCommand ??= new DelegateCommand(this.TestAuth);
+
+        private void TestAuth(object commandParameter)
+        {
+            // Authorize with Google Photos
+        }
+    }
+}
