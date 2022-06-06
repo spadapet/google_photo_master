4a4b573c2194fe691786d436899ed4aff69e370b
Peter Spada <spadapet@hotmail.com>
2022-06-06 14:32:38 -0700

    Add PhotoHelper

---
+++ PhotoMaster/PhotoHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CasCap.Models;
+using CasCap.Services;
+
+namespace PhotoMaster
+{
+    internal class PhotoHelper
+    {
+        private readonly GooglePhotosService photos;
+        private const string SpecialAlbumName = "Can delete to save space";
+
+        public PhotoHelper(GooglePhotosService photos)
+        {
+            this.photos = photos;
+        }
+
+        public async Task Run(CancellationToken token)
+        {
+            if (!await this.photos.LoginAsync())
+            {
+                throw new Exception("Unable to log in.");
+            }
+
+            HashSet<string> itemsInAlbums = new HashSet<string>();
+            HashSet<string> itemsInSpecialAlbum = new HashSet<string>();
+            HashSet<string> itemsToAddToSpecialAlbum = new HashSet<string>();
+            HashSet<string> itemsToRemoveFromSpecialAlbum = new HashSet<string>();
+            List<Album> albums = await this.photos.GetAlbumsAsync(cancellationToken: token);
+            Album specialAlbum = albums.FirstOrDefault(a => a.title == PhotoHelper.SpecialAlbumName);
+
+            foreach (Album album in albums)
+            {
+                token.ThrowIfCancellationRequested();
+
+                Console.WriteLine($"Fetching album: {album.title} ({album.mediaItemsCount} items)");
+
+                List<MediaItem> items = await this.photos.GetMediaItemsByAlbumAsync(album.id, cancellationToken: token);
+                foreach (MediaItem item in items)
+                {
+                    (album == specialAlbum ? itemsInSpecialAlbum : itemsInAlbums).Add(item.id);
+                }
+            }
+
+            Console.WriteLine($"Fetching all media items");
+
+            List<MediaItem> allItems = await this.photos.GetMediaItemsByDateRangeAsync(new DateTime(2021, 6, 1), DateTime.Now, cancellationToken: token);
+            foreach (MediaItem item in allItems)
+            {
+                if (!itemsInAlbums.Contains(item.id))
+                {
+                    if (!itemsInSpecialAlbum.Contains(item.id))
+                    {
+                        if (itemsToAddToSpecialAlbum.Add(item.id))
+                        {
+                            // Console.WriteLine($"Can be deleted: {item.filename}");
+                        }
+                    }
+                }
+                else if (itemsInSpecialAlbum.Contains(item.id))
+                {
+                    if (itemsToRemoveFromSpecialAlbum.Add(item.id))
+                    {
+                        // Console.WriteLine($"Should not delete anymore: {item.filename}");
+                    }
+                }
+            }
+
+            if (itemsToAddToSpecialAlbum.Count > 0)
+            {
+                Console.WriteLine($"Marking {itemsToAddToSpecialAlbum.Count} items that can be deleted");
+
+                if (specialAlbum == null)
+                {
+                    specialAlbum = await this.photos.GetOrCreateAlbumAsync(PhotoHelper.SpecialAlbumName);
+                }
+
+                if (!await this.photos.AddMediaItemsToAlbumAsync(specialAlbum.id, itemsToAddToSpecialAlbum.ToArray()))
+                {
+                    Console.WriteLine($"Failed to add items");
+                }
+            }
+
+            if (itemsToRemoveFromSpecialAlbum.Count > 0)
+            {
+                Console.WriteLine($"Unmarking {itemsToRemoveFromSpecialAlbum.Count} items that shouldn't be deleted anymore");
+
+                if (!await this.photos.RemoveMediaItemsFromAlbumAsync(specialAlbum.id, itemsToRemoveFromSpecialAlbum.ToArray()))
+                {
+                    Console.WriteLine($"Failed to remove items");
+                }
+            }
+        }
+    }
+}
--- PhotoMaster/Program.cs
+++ PhotoMaster/Program.cs
@@ -1,61 +1,44 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
+﻿using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
-using CasCap.Models;
-using CasCap.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace PhotoMaster
 {
-    internal static class Program
+    internal class Program
     {
-        private const int ResultSuccess = 0;
-        private const int ResultError = 1;
-
-        internal async static Task<int> Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            int result = Program.ResultSuccess;
-            const string userEmail = "lilapeterspada@gmail.com";
-
-            IOptions<GooglePhotosOptions> options = Options.Create(new GooglePhotosOptions()
+            using (CancellationTokenSource tokenSource = new CancellationTokenSource())
             {
-                User = userEmail,
-                ClientId = "012345678901-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.apps.googleusercontent.com",
-                ClientSecret = "abcabcabcabcabcabcabcabc",
-                Scopes = new[] { GooglePhotosScope.Access },
-            });
-
-            using (HttpClientHandler handler = new HttpClientHandler()
-            {
-                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-            })
-            using (HttpClient client = new HttpClient(handler))
-            using (LoggerFactory loggerFactory = new LoggerFactory())
-            {
-                ILogger<GooglePhotosService> logger = loggerFactory.CreateLogger<GooglePhotosService>();
-                GooglePhotosService photos = new GooglePhotosService(logger, options, client);
-
-                try
-                {
-                    if (!await photos.LoginAsync())
-                    {
-                        throw new Exception();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    if (!string.IsNullOrEmpty(ex.Message))
-                    {
-                        logger.LogError(ex);
-                    }
-
-                    result = Program.ResultError;
-                }
+                IHost host = Program.CreateHostBuilder(args).Build();
+                await host.Services.GetService<PhotoHelper>().Run(tokenSource.Token);
             }
+        }
 
-            return result;
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.SetBasePath(Directory.GetCurrentDirectory());
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddGooglePhotos();
+                    services.AddSingleton<PhotoHelper>();
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddConsole();
+                })
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddUserSecrets<Program>();
+                });
         }
     }
 }
